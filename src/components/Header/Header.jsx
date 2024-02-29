10893b79f231f27f7ef9032074e5d570aed78df1
Ever Henry Abanto Vilchez <eabantov20_1@unc.edu.pe>
2024-02-29 17:19:43 -0500

    Actualizacion contactanos

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import './Header.css'
-import logo from '../../image/logo.png'
+import logo from '../../image/logoEM.jpeg'
 import userDefault from '../../image/user_img.avif'
 import { Link } from 'react-router-dom'
 import { useAuth0 } from '@auth0/auth0-react'
